d2ad7e91ff3f9b1c701c97d9088ddb5a2b919ef5
Luke <lhensley@mail.bradley.edu>
2022-03-14 23:51:23 -0500

    Assignment

---
+++ Assignment5B/Assets/MyFirstPersonPlayer/Scripts/ShootWithRaycasts.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * (Luke Hensley)
+ * (Assignment 5B)
+ * (Controls the shooting with raycasts)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootWithRaycasts : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+
+    public float hitForce = 10f;
+
+    public ParticleSystem flash;
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        flash.Play();
+        RaycastHit hitInfo;
+
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, range))
+        {
+
+            Debug.Log(hitInfo.transform.gameObject.name);
+
+
+       
+            Target target = hitInfo.transform.gameObject.GetComponent<Target>();
+
+            if(target != null)
+            {
+                target.TakeDamage(damage);
+                    
+            }
+
+            if(hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(cam.transform.TransformDirection(Vector3.forward) * hitForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
---
+++ Assignment5B/Assets/MyFirstPersonPlayer/Scripts/Target.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * (Luke Hensley)
+ * (Assignment 5B)
+ * (Controls health of targets)
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+       if(health < 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
